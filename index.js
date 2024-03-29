2ff841a1710bba2ade28f4d0c1ed98788765847a
wanjiruth <njoki.ruth.wanjiku@gmail.com>
2024-03-29 17:07:52 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(superbowls) {
+    const winningYear = superbowls.find(superbowl => superbowl.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }
+  
+  
+  const superbowls = [
+    { year: 2020, result: "W" },
+    { year: 2021, result: "L" },
+    { year: 2022, result: "W" },
+    { year: 2023, result: "L" }
+  ];
+  
+  console.log(superbowlWin(superbowls)); 
+  
